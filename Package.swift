07137ad9c866cfc12c82dd2349d2d1352833128d
Borko Tomić <borko.tomic@avawomen.com>
2020-11-21 02:41:36 +0100

    Fixing dependecies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CommonCore",
+    platforms: [
+         .iOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -22,8 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommonCore",
-            dependencies: ["Lottie"]
-        ),
+            dependencies: ["Lottie"]),
         .testTarget(
             name: "CommonCoreTests",
             dependencies: ["CommonCore"]),
