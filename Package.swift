d0773204e5ef8f03b6b3a13c6116cf0174c6eee8
Borko Tomić <borko.tomic@avawomen.com>
2020-11-21 02:33:13 +0100

    Changing lottie name in dependencies

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommonCore",
-            dependencies: ["lottie-ios"]
+            dependencies: ["Lottie"]
         ),
         .testTarget(
             name: "CommonCoreTests",
